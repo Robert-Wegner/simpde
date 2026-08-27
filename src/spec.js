2f1fa9687666798da647d7c6d7726d7d962f8685
Robert Wegner <robert.wegner4@outlook.de>
2026-08-27 15:34:45 +0200

    Set eight millisecond default frame interval

--- src/spec.js
+++ src/spec.js
@@ -119,7 +119,7 @@ const defaultSpecification = {
         },
         {
             name: "delay", displayName: "Minimum frame interval", group: "/settings/sim",
-            type: "int", minimum: 0, defaultValue: 10,
+            type: "int", minimum: 0, defaultValue: 8,
             restartOnChange: true,
             description: "The timer interval between displayed frames, in milliseconds."
         },
